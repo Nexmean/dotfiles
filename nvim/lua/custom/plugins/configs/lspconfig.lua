5985923e311bf196afc4a32da92d32c86555c65a
Makarov Aleksandr S <al.makarov@tinkoff.ru>
2022-07-17 00:28:05 +0300

    neovide: NvChad: fix lspconfig for haskell

--- nvim/lua/custom/plugins/configs/lspconfig.lua
+++ nvim/lua/custom/plugins/configs/lspconfig.lua
@@ -3,14 +3,28 @@ local util = require "lspconfig.util"
 local M = {}
 
 M.setup_lsp = function (attach, capabilities)
-   local lspconfig = require('lspconfig')
+   local lspconfig = require("lspconfig")
 
    lspconfig["hls"].setup {
       on_attach = function (client, bufnr)
          attach(client, bufnr)
       end,
       capabilities = capabilities,
-      root_dir = util.root_pattern('hie.yaml', 'cabal.project', 'stack.yaml'),
+      root_dir = function (startpath)
+         local project_patterns = { "hie.yaml", "cabal.project", "stack.yaml" }
+         local cwd = vim.fn.getcwd()
+
+         for _, pattern in ipairs(project_patterns) do
+            for _, p in ipairs(vim.fn.glob(util.path.join(cwd, pattern), true, true)) do
+               if util.path.exists(p) then
+                  return cwd
+               end
+            end
+         end
+
+         local all_patterns = vim.tbl_flatten({ project_patterns, { "*.cabal", "package.yaml" } })
+         return util.root_pattern(table.unpack(all_patterns))(startpath)
+      end,
       settings = {
          haskell = {
             formattingProvider = "stylish-haskell"
