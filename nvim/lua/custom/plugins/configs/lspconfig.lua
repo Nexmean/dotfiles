68d9eae1dc2ba0d0fedd4af02631b5a30f142f40
Makarov Aleksandr S <al.makarov@tinkoff.ru>
2022-07-18 14:03:50 +0300

    neovim: NvChad: remove unnecessary lspconfig lines due to accepted PR https://github.com/neovim/nvim-lspconfig/pull/2010

--- nvim/lua/custom/plugins/configs/lspconfig.lua
+++ nvim/lua/custom/plugins/configs/lspconfig.lua
@@ -1,27 +1,9 @@
-local util = require "lspconfig.util"
-
 local M = {}
 
 M.setup_lsp = function (attach, capabilities)
    local lspconfig = require("lspconfig")
 
-   lspconfig["hls"].setup {
-      on_attach = function (client, bufnr)
-         attach(client, bufnr)
-      end,
-      capabilities = capabilities,
-      root_dir = function (filepath)
-         return (util.root_pattern("hie.yaml", "cabal.project", "stack.yaml")(filepath)
-            or util.root_pattern("*.cabal", "package.yaml")(filepath))
-      end,
-      settings = {
-         haskell = {
-            formattingProvider = "stylish-haskell"
-         }
-      }
-   }
-
-   local default_servers = {"jsonls"}
+   local default_servers = {"jsonls", "hls"}
 
    for _, lsp in ipairs(default_servers) do
       lspconfig[lsp].setup {
