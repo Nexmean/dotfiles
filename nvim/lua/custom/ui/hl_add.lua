3991fda9a265f8aa8c32ba4a5df02af1c34ae36f
Alexandr Makarov <kremovtort@kremov_pc.localdomain>
2022-09-18 23:35:23 +0300

    feat(neovim): neogit highlighting

---
+++ nvim/lua/custom/ui/hl_add.lua
@@ -0,0 +1,45 @@
+return {
+   -- Neogit
+   NeogitNotificationInfo = {
+      fg = "green",
+   },
+   NeogitNotificationWarning = {
+      fg = "yellow",
+   },
+   NeogitNotificationError = {
+      fg = "red",
+   },
+
+   NeogitDiffAdd = {
+      bg = "black",
+      fg = "green",
+   },
+   NeogitDiffAddHighlight = {
+      bg = "lightbg",
+      fg = "green",
+   },
+   NeogitDiffDelete = {
+      bg = "black",
+      fg = "red",
+   },
+   NeogitDiffDeleteHighlight = {
+      bg = "lightbg",
+      fg = "red",
+   },
+   NeogitDiffContextHighlight = {
+      bg = "black2",
+   },
+   NeogitHunkHeader = {
+      bg = "lightbg",
+   },
+   NeogitHunkHeaderHighlight = {
+      bg = "grey",
+   },
+   NeogitCommitViewHeader = {
+      bg = "teal",
+      fg = "black",
+   },
+   NeogitFilePath = {
+      fg = "dark_purple"
+   },
+}
