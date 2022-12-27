a11de0203763ef696d21d846e420493dec856d69
Alexander Makarov <next.meaning@gmail.com>
2022-12-28 00:43:35 +0300

    improve(neovim): nolinebreak in unfocused windows

--- nvim/lua/user/autocommands.lua
+++ nvim/lua/user/autocommands.lua
@@ -9,3 +9,21 @@ autocmd({"BufEnter", "BufWinEnter"}, {
     end
   end
 })
+
+autocmd({ "WinEnter" }, {
+  callback = function ()
+    if vim.o.bt == "" then
+      vim.o.wrap = true
+      vim.o.linebreak = true
+    end
+  end
+})
+
+autocmd({ "WinLeave" }, {
+  callback = function ()
+    if vim.o.bt == "" and vim.o.ft ~= "markdown" then
+      vim.o.wrap = false
+      vim.o.linebreak = false
+    end
+  end
+})
