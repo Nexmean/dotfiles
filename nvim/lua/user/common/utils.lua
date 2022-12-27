0b8e6cb4f147cd13fb5edb3d36ad600165b781ed
Alexander Makarov <next.meaning@gmail.com>
2022-12-28 00:45:08 +0300

    chore(neovim): add tables serialization utility for debug purposes

--- nvim/lua/user/common/utils.lua
+++ nvim/lua/user/common/utils.lua
@@ -1192,4 +1192,17 @@ function M.cmdfn(command)
   end
 end
 
+function M.dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. M.dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
+
 return M
