3b35add96fcae82e4d155bf4b6f0644ad3c2fd3f
Alexander Makarov <next.meaning@gmail.com>
2022-12-31 20:04:12 +0300

    feat: add starship and wezterm configs

---
+++ .wezterm.lua
@@ -0,0 +1,23 @@
+local wezterm = require 'wezterm'
+
+return {
+  font = wezterm.font 'JetbrainsMono Nerd Font',
+  font_size = 16,
+  color_scheme = 'Catppuccin Mocha',
+  default_cursor_style = 'SteadyBar',
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
+  term = "wezterm",
+  hide_tab_bar_if_only_one_tab = true,
+  detect_password_input = true,
+  initial_cols = 140,
+  initial_rows = 40,
+  use_resize_increments = true,
+  use_fancy_tab_bar = false,
+  tab_max_width = 20,
+  max_fps = 120,
+}
